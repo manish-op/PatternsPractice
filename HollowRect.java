9bad0810f2291158adab090e0ec3e071bef342df
Manish <manish402699@gmail.com>
2025-04-18 00:32:35 +0530

    Second Pattern

--- HollowRect.java
+++ HollowRect.java
@@ -2,14 +2,14 @@ package PatternsPractice;
 
 public class HollowRect {
     public static void main(String[] args) {
-        int n=10;  // rows
-        int m=10;
+        int n=15;  // rows
+        int m=5;   //columns
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
+            for(int j=1;j<=m;j++){
                 if(i==1 || j==1 || i==n || j==m){
-                    System.out.print("*");
+                    System.out.print("* ");
                 }else {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
 
             }
