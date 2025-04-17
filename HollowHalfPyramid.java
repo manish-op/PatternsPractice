8de9adfb29a821ba5abdd799ac4ed82685e85d4e
Manish <manish402699@gmail.com>
2025-04-18 01:03:01 +0530

    Fourth Pattern

--- HollowHalfPyramid.java
+++ HollowHalfPyramid.java
@@ -1,4 +1,20 @@
 package PatternsPractice;
 
 public class HollowHalfPyramid {
+    public static void main(String[] args){
+
+        int rows = 5;  //  number of rows
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                // Print '*' if it's the first or last column, or the last row
+                if (j == 1 || j == i || i == rows) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");  // Space for hollow effect
+                }
+            }
+            System.out.println();  // Move to next line
+        }
+    }
 }
