eacf538ab3d734e4828ac375204735dafbeb0f0a
Manish <manish402699@gmail.com>
2025-04-18 01:04:05 +0530

    Firth pattern

--- InvertedPyramid.java
+++ InvertedPyramid.java
@@ -1,4 +1,13 @@
 package PatternsPractice;
 
 public class InvertedPyramid {
+    public static void main(String[] args){
+        int n=6;
+        for (int i=n;i>0;i--){
+            for (int j = 1; j<i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
