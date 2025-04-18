16a079736b3f8bb6ee376511e15f1d66a78b5daf
Manish <manish402699@gmail.com>
2025-04-18 15:25:04 +0530

    #

--- InvertedPyramid.java
+++ InvertedPyramid.java
@@ -3,11 +3,23 @@ package PatternsPractice;
 public class InvertedPyramid {
     public static void main(String[] args){
         int n=6;
-        for (int i=n;i>0;i--){
-            for (int j = 1; j<i ; j++) {
+        for (int i=n; i>0; i--){
+            for (int j = 0; j<i ; j++) {
                 System.out.print("*");
             }
             System.out.println();
+        }
+
+
+        System.out.println("*********************");
+        System.out.println();
+
+        for (int i = 5 ; i > 0 ; i--) {
+            for (int j = i; j >0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+
         }
     }
 }
