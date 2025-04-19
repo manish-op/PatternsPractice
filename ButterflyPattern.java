d691ad1bf4727045fff41441e53388acc5900056
Manish <manish402699@gmail.com>
2025-04-20 01:54:00 +0530

    ButterflyPattern

---
+++ ButterflyPattern.java
@@ -0,0 +1,105 @@
+package PatternsPractice;
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+
+        int n=5;
+        //upper half
+        for (int i=1;i<=n;i++){
+            //for stars
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(" * ");
+            }
+            //for spaces
+            int spaces=2*(n-i);
+            for (int j=1; j<=spaces;j++){
+                System.out.print(" . ");
+            }
+            //for stars
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+        //lower half
+
+
+        for (int i=n;i>=1;i--){
+            //for stars
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(" * ");
+            }
+            //for spaces
+            int spaces=2*(n-i);
+            for (int j=1; j<=spaces;j++){
+                System.out.print(" . ");
+            }
+            //for stars
+            for (int j = 1; j <=i ; j++) {
+                System.out.print( " * ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(".............................");
+
+        //printing hollow butterfly
+
+        int rows=5;
+        //upper half
+        for (int i=1;i<=rows;i++){
+            //for stars
+            for (int j = 1; j <=i ; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            //for spaces
+            int spaces=2*(rows-i);
+            for (int j=1; j<=spaces;j++){
+                System.out.print(" ");
+            }
+            //for stars
+            for (int j = 1; j <=i ; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+
+        //lower half
+
+
+        for (int i=rows;i>=1;i--){
+            //for stars
+            for (int j = 1; j <=i ; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            //for spaces
+            int spaces=2*(rows-i);
+            for (int j=1; j<=spaces;j++){
+                System.out.print(" ");
+            }
+            //for stars
+            for (int j = 1; j <=i ; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
