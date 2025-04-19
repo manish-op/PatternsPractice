be6a0023039c5d22a197462561a38ea431cd356e
Manish <manish402699@gmail.com>
2025-04-19 16:24:12 +0530

    <>

--- FlyodTriangle.java
+++ FlyodTriangle.java
@@ -5,8 +5,8 @@ public class FlyodTriangle {
         int num=5;
         int n=1;
         for(int i=1;i<=num;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(n+" ");
+            for(int j=1;j<=i;j++) {
+                System.out.print(n + " ");
                 n++;
             }
             System.out.println();
--- FullPyramid.java
+++ FullPyramid.java
@@ -11,8 +11,24 @@ public class FullPyramid {
             for (int j = i; j < n-1; j++) {
                 System.out.print(" ");
             }
-
+            //for stars
             for (int k = 0; k < 2*i+1 ; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("##############################");
+
+        // inverted pyramid
+
+        int m=5;
+        for (int i = m; i >=1 ; i--) {
+            //for spaces
+            for (int j = 5; j>i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1 ; j <=2*i-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
