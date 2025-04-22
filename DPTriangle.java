5891962ef830fffbbb936630b0cb3df770ba6559
Manish <manish402699@gmail.com>
2025-04-22 15:47:37 +0530

    DPTriangle

--- DPTriangle.java
+++ DPTriangle.java
@@ -2,7 +2,7 @@ package PatternsPractice;
 
 public class DPTriangle {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 3;
 
         // First part
         for (int i = 0; i < n / 2 + 2; i++) {
@@ -63,7 +63,51 @@ public class DPTriangle {
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        
+
+        int nr, i, j;
+        nr=3;
+
+       //upper part
+
+        for ( i = 0; i <(n/2)+2 ; i++) {
+            for (j=0;j<n-i;j++){
+                System.out.print("_");
+            }
+            for (j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
+
+        //2nd part
+        for ( i = 0; i < n; i++) {
+            //middle-left part
+            for ( j = 0; j <n/2+1 ; j++) {
+                if (j>=n/2-i && j>=i-n/2){
+                    System.out.print("*");
+                }else System.out.print("_");
+            }
+
+            for ( j = 0; j <n ; j++) {
+                if (i==n/2){
+                    System.out.print("*");
+                }else System.out.print("_");
+            }
+
+            for ( j = 0; j <(n/2)+1 ; j++) {
+                if(j>=n/2-i && j>=i-n/2){
+                    System.out.print("*");
+                }else System.out.print("_");
+
+            }
+            System.out.println();
+        }
+    }
 
 
     }
