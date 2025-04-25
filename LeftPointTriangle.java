05e76bdb4f9f530986fd8d6f632a2ec127b31cf8
Manish <manish402699@gmail.com>
2025-04-26 01:38:38 +0530

    pointed-triangle

--- LeftPointTriangle.java
+++ LeftPointTriangle.java
@@ -2,13 +2,14 @@ package PatternsPractice;
 
 public class LeftPointTriangle {
     public static void main(String[] args){
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j <i ; j++) {
+        int n=5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i ; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-        for (int i=5; i>0; i--){
+        for (int i=n-1; i>0; i--){
             for (int j = 0; j<i ; j++) {
                 System.out.print("* ");
             }
