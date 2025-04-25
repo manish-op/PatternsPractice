f402a6e8edcca1581fca9f8cb321d4b4b267e41f
Manish <manish402699@gmail.com>
2025-04-26 01:13:39 +0530

    hollow-rhombus

--- HollowRhombus.java
+++ HollowRhombus.java
@@ -7,7 +7,7 @@ public class HollowRhombus {
         for (int i=0;i<n;i++){
             //for spaces
             for (int j = 0; j < n-i; j++) {
-                System.out.print(" ");
+                System.out.print(".");
             }
 
             // for stars
@@ -15,7 +15,7 @@ public class HollowRhombus {
                 if (i == 0 || i == n-1 || j == 0 || j == n-1) {
                     System.out.print("* ");
                 } else {
-                    System.out.print("  ");
+                    System.out.print(". ");
                 }
             }
             System.out.println();
