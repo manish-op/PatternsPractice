792d3451a759e89366948d4f1d51fc9221ae9133
Manish <manish402699@gmail.com>
2025-04-26 00:01:21 +0530

    ButterFlyy

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -7,16 +7,16 @@ public class ButterflyPattern {
         //upper half
         for (int i=1;i<=n;i++){
             //for stars
-            for (int j = 1; j <=i ; j++) {
+            for (int j = 0; j<i ; j++) {
                 System.out.print(" * ");
             }
             //for spaces
             int spaces=2*(n-i);
-            for (int j=1; j<=spaces;j++){
+            for (int j=0; j<spaces;j++){
                 System.out.print(" . ");
             }
             //for stars
-            for (int j = 1; j <=i ; j++) {
+            for (int j = 0 ;j <i ; j++) {
                 System.out.print(" * ");
             }
             System.out.println();
@@ -27,16 +27,16 @@ public class ButterflyPattern {
 
         for (int i=n;i>=1;i--){
             //for stars
-            for (int j = 1; j <=i ; j++) {
+            for (int j = 0; j <i ; j++) {
                 System.out.print(" * ");
             }
             //for spaces
             int spaces=2*(n-i);
-            for (int j=1; j<=spaces;j++){
+            for (int j=0; j<spaces;j++){
                 System.out.print(" . ");
             }
             //for stars
-            for (int j = 1; j <=i ; j++) {
+            for (int j = 0; j <i ; j++) {
                 System.out.print( " * ");
             }
             System.out.println();
