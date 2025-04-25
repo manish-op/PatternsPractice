55ffdaf658633c5699c78dbff778ced5e9bcb1e7
Manish <manish402699@gmail.com>
2025-04-26 00:32:53 +0530

    Flyods Triangle

--- FlyodTriangle.java
+++ FlyodTriangle.java
@@ -4,6 +4,7 @@ public class FlyodTriangle {
     public static void main(String[] args){
         int num=5;
         int n=1;
+
         for(int i=1;i<=num;i++){
             for(int j=1;j<=i;j++) {
                 System.out.print(n + " ");
